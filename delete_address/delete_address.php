8234c238c6f6302c483c3578253d070150c68661
smimoon <simonlaroche@orange.fr>
2026-02-18 15:00:10 +0000

    étape 8 : delete address

---
+++ delete_address/delete_address.php
@@ -0,0 +1,10 @@
+<?php
+    require "../connexion.php";
+    $id = $_POST['id'];
+    
+    $query = $db->prepare("DELETE FROM address WHERE id = :id");
+    $parameters = [
+        'id'=> $id
+    ];
+    $query->execute($parameters);
+?>
