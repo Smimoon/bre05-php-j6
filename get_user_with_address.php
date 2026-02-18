159f980c3d801743df790b3229b75cd4db82ac98
smimoon <simonlaroche@orange.fr>
2026-02-18 13:05:04 +0000

    étape 4 : get user with address

---
+++ get_user_with_address.php
@@ -0,0 +1,14 @@
+<?php
+    require "connexion.php";
+    $query = $db->prepare('SELECT users.first_name, users.last_name, address.street, address.city, address.zipcode FROM users JOIN address ON users.address = address.id WHERE users.id = :id');
+    $parameters = [
+        'id'=>$_GET['id'] 
+    ];
+    
+    $query->execute($parameters);
+
+    $user = $query->fetch(PDO::FETCH_ASSOC);
+    
+    var_dump($user);
+
+?>
